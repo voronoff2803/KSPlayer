e1e673a40c68ffb71e4cc7926a140431e61cf5c5
kintan <kingslay@icloud.com>
2024-05-12 15:09:21 +0800

    fix test

--- Tests/KSPlayerTests/SubtitleTest.swift
+++ Tests/KSPlayerTests/SubtitleTest.swift
@@ -2,6 +2,7 @@
 import XCTest
 
 extension XCTestCase {
+    // 用swift test的话无法返回url
     func testURLForResource(_ resourceName: String) -> URL {
         Bundle(url: Bundle(for: type(of: self)).url(forResource: "KSPlayer_KSPlayerTests.bundle", withExtension: nil)!)!.url(forResource: resourceName, withExtension: nil)!
     }
@@ -65,15 +66,25 @@ class SubtitleTest: XCTestCase {
     }
 
     func testSrt2() async {
-        let url = testURLForResource("Sons.of.Anarchy.S02E05.txt")
-        let string = try? await url.string()
-        let scanner = Scanner(string: string!)
+        let string = """
+        1
+        00:00:06,886 --> 00:00:08,569
+        嘿
+        <font size="8px">Hey.</font>
+
+        2
+        00:00:09,419 --> 00:00:10,569
+        早上好
+        <font color="#4096d1">本字幕仅供学习交流，严禁用于商业用途</font>
+
+        """
+        let scanner = Scanner(string: string)
         let parse = SrtParse()
         XCTAssertEqual(parse.canParse(scanner: scanner), true)
         let parts = parse.parse(scanner: scanner) as! [SubtitlePart]
-        XCTAssertEqual(parts.count, 6)
-        XCTAssertEqual(parts[4].render.right?.string.contains("<"), false)
-        XCTAssertEqual(parts[5].render.right?.string.contains("<"), false)
+        XCTAssertEqual(parts.count, 2)
+        XCTAssertEqual(parts[0].render.right?.string.contains("<"), false)
+        XCTAssertEqual(parts[1].render.right?.string.contains("<"), false)
     }
 
     func testVtt() {
